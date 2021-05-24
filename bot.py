be99fcda5cfaa0cc8fba218e9d558e67fb6ce6df
JMcB <joel.mcbride1@live.com>
2021-05-24 09:45:54 +0100

    Create files

---
+++ bot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import discord.ext.commands
+
+
+class CuboidBot(discord.ext.commands.Bot):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
