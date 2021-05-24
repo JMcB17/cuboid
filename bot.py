779befa324e1b04e3d1e7f89ec5f8521cda3ff9e
JMcB <joel.mcbride1@live.com>
2021-05-24 11:23:56 +0100

    Create bot stub

--- bot.py
+++ bot.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 
+import json
+import argparse
+from pathlib import Path
+
 import discord.ext.commands
 
 
+__version__ = '0.1.0'
+
+
+CONFIG_PATH = Path('config.json')
+
+
 class CuboidBot(discord.ext.commands.Bot):
-    pass
+    def __init__(self, config, *args, **kwargs):
+        self.config = config
+
+        parser = argparse.ArgumentParser()
+        self.parser = parser
+
+        super().__init__(command_prefix='cuboid', *args, **kwargs)
+
+    async def on_message(self, message):
+        args = self.parser.parse_args(message.split())
+        print(args)
+
+        await super().on_message(message)
 
 
 def main():
-    pass
+    with open(CONFIG_PATH) as config_file:
+        config = json.load(config_file)
+
+    bot = CuboidBot(config)
+    bot.run(config['discord_token'])
 
 
 if __name__ == '__main__':
