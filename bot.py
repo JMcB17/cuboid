aecdb0e0e07b236e443df94cd0c5b6cec3c0e4a7
JMcB <joel.mcbride1@live.com>
2021-05-24 12:15:01 +0100

    Add args for instal command

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ from pathlib import Path
 import discord.ext.commands
 
 
+# todo: hack the help command, after making real functionality which is a priority
+# todo: make this discord.py/argparse mashup a library if it comes out well?
+
+
 __version__ = '0.1.0'
 
 
@@ -21,14 +25,21 @@ class CuboidBot(discord.ext.commands.Bot):
         parser = argparse.ArgumentParser(
             prog=self.name,
             description='Bot manager for Discord',
-            exit_on_error=False
+            exit_on_error=False,
+            add_help=False
         )
         parser.add_argument('--version', action='store_true')
         subparsers = parser.add_subparsers(dest='subcommand')
-        parser_version = subparsers.add_parser(
+        subparsers.add_parser(
             'version',
             help="show program's version number and exit"
         )
+        parser_install = subparsers.add_parser(
+            'install',
+            help='add bots to the server'
+        )
+        parser_install.add_argument('bots', nargs='+', help='the bots to add')
+        parser_install.add_argument('-y', '--yes', action='store_true', help='do not ask for confirmation')
         self.parser = parser
 
         super().__init__(command_prefix=self.name, *args, **kwargs)
@@ -42,11 +53,15 @@ class CuboidBot(discord.ext.commands.Bot):
             try:
                 args = self.parser.parse_args(argv)
             except argparse.ArgumentError:
+                # todo: add log here
                 pass
             else:
                 print(args)
                 if args.version or args.subcommand == 'version':
                     await message.channel.send(__version__)
+                elif args.subcommand == 'install':
+                    bots = [b.replace('_', ' ') for b in args.bots]
+                    await message.channel.send(bots)
 
         await super().on_message(message)
 
